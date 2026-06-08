d1232e9c56a72deea74490ae582ce3224527a4bf
Muhammad Mujahidin Eko Prasetyo <158280679+mujahidcorporation@users.noreply.github.com>
2026-06-09 02:56:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function bukaAplikasi(url){
-    document.querySelector(".container").style.display = "none";
-    document.getElementById("viewer").style.display = "flex";
-
-    // IMPORTANT: paksa reload clean URL
-    document.getElementById("frame").src = "about:blank";
-
-    setTimeout(()=>{
-        document.getElementById("frame").src = url;
-    },100);
-}
-
-function kembaliMenu(){
-    document.querySelector(".container").style.display = "block";
-    document.getElementById("viewer").style.display = "none";
-    document.getElementById("frame").src = "";
+function bukaWeb(url){
+    window.open(url, "_blank");
 }
